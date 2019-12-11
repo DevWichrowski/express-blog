edee977bbdb6f193089d90de59e9d7aa9bf70270
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-12-11 09:29:36 +0100

    $in

--- controllers/posts.js
+++ controllers/posts.js
@@ -64,7 +64,7 @@ exports.patch_post = async (req, res) => {
 
 exports.get_related_posts = async (req, res) => {
     try {
-        const posts = await Post.find({ 'tags.value' : {$all: ['react', 'js']}});
+        const posts = await Post.find({'tags.value': {$in: req.body.tags}});
         console.log('req.params.tags', req.body.tags)
         console.log('posts', posts);
         res.json(posts);
