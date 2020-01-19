e65d8196d4132cad6e6cab18bfd922303ba77005
devwichrowski <damian1492>
2020-01-19 21:16:36 +0100

    Docker changes

--- frontend/src/core/api.js
+++ frontend/src/core/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const HOST = "http://localhost:3000";
+export const HOST = "http://localhost:8000";
 
 export const getAllPostsApi = () => {
     return axios.get(`${HOST}/posts`, {
