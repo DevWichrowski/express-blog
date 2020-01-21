01650955cba9737b9681b9a5ce5376ede7fb4167
DevWichrowski <wichrowski.patryk92@gmail.com>
2020-01-21 21:22:06 +0100

    Few changes

--- frontend/src/components/DevelopRegister/DevelopRegister.js
+++ frontend/src/components/DevelopRegister/DevelopRegister.js
@@ -4,6 +4,7 @@ import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import {connect} from "react-redux";
 import {registerUserPending} from "../../store/actions/users.actions";
+import Avatar from "@material-ui/core/Avatar";
 
 const DevelopRegister = props => {
     const [newUser, setNewUser] = useState({});
@@ -30,6 +31,13 @@ const DevelopRegister = props => {
             <h1>Register</h1>
             <form className="register-develop-form">
                 <div className="register-develop-container">
+
+                    <div className="avatar-container">
+                        <Avatar
+                            className="avatar"
+                            src={newUser.avatarUrl ?? `https://byuc.files.wordpress.com/2012/07/avat-2.jpg`}
+                            alt={newUser.nickname ?? 'User avatar'}/>
+                    </div>
                     <TextField className="text-field" id="standard-basic" variant="outlined" label="Login"
                                onChange={e => handleValues(e, 'login')}/>
                     <TextField className="text-field" id="standard-basic" variant="outlined" label="Nickname"
@@ -37,7 +45,7 @@ const DevelopRegister = props => {
                     <TextField className="text-field" id="standard-basic" variant="outlined" label="Email"
                                onChange={e => handleValues(e, 'email')}/>
                     <TextField className="text-field" id="standard-basic" variant="outlined" label="Avatar URL"
-                               onChange={e => handleValues(e, 'avatar')}/>
+                               onChange={e => handleValues(e, 'avatarUrl')}/>
                     <TextField className="text-field" id="standard-basic" variant="outlined" label="Password"
                                onChange={e => handleValues(e, 'password')} type="password"/>
                     <TextField className="text-field" id="standard-basic" variant="outlined" label="Re-type password"
